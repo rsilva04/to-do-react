4c617fad91038e7254cd2da7a946ab4638229dfb
Rafael Silva <rafsilva004@gmail.com>
2023-08-19 20:33:47 -0500

    created GET route

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,6 +3,18 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+router.get('/', (req, res) => {
+    const sqlText = `SELECT * FROM toDo;`;
+    pool.query(sqlText)
+        .then ((result) => {
+            console.log(`Got stuff back from database`, result);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500);
+        })
+})
 
 // POST
 
